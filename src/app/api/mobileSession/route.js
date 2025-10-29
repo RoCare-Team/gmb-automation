828a88f04359981dcc2414448f8dbdc6c2434c4d
webDevSociapa <webdev@sociapa.com>
2025-10-29 15:02:32 +0530

    fixes

--- src/app/api/mobileSession/route.js
+++ src/app/api/mobileSession/route.js
@@ -1,10 +1,10 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "../auth/[...nextauth]/route";
 
-export default async function handler(req, res) {
-  const session = await getServerSession(req, res, authOptions);
-  if (!session) {
-    return res.status(401).json({ error: "Not logged in" });
-  }
-  res.json(session); // includes accessToken, refreshToken, email, etc.
-}
+// export default async function handler(req, res) {
+//   const session = await getServerSession(req, res, authOptions);
+//   if (!session) {
+//     return res.status(401).json({ error: "Not logged in" });
+//   }
+//   res.json(session); // includes accessToken, refreshToken, email, etc.
+// }
