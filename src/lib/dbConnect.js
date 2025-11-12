961949f7a3e95251c9cd4adea4c86c00b5e15c1c
webDevSociapa <webdev@sociapa.com>
2025-11-12 19:12:17 +0530

    fixess

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -4,6 +4,7 @@ const MONGO_URI = process.env.MONGODB_URI;
 
 if (!MONGO_URI) throw new Error('MONGO_URI not defined');
 
+
 let cached = global.mongoose;
 if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
